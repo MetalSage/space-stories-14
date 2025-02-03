5de44403f70a52c22d8e26ead209b9e090ea9e22
BeBright <98597725+be1bright@users.noreply.github.com>
2025-02-03 23:08:11 +0300

    cards

---
+++ Content.Client/_Stories/Cards/CardSystemClient.cs
@@ -0,0 +1,13 @@
+using Robust.Client.GameObjects;
+using Robust.Shared.GameStates;
+using Content.Shared._Stories.Cards.Fan;
+using Robust.Shared.Utility;
+
+namespace Content.Client._Stories.Cards;
+public sealed class CardSystemClient : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
