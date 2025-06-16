cad04abebdccb89ee72308ff0ca7ec4cb2156f82
Doublechest <twin01v@gmail.com>
2025-06-16 23:53:08 +0300

    ContactInteractionEvent inventory relay

--- Content.Shared/Interaction/Events/ContactInteractionEvent.cs
+++ Content.Shared/Interaction/Events/ContactInteractionEvent.cs
@@ -1,3 +1,5 @@
+using Content.Shared.Inventory; // Stories
+
 namespace Content.Shared.Interaction.Events;
 
 /// <summary>
@@ -6,9 +8,10 @@ namespace Content.Shared.Interaction.Events;
 /// <remarks>
 ///     This is currently used by the forensics and disease systems to perform on-contact interactions.
 /// </remarks>
-public sealed class ContactInteractionEvent : HandledEntityEventArgs
+public sealed class ContactInteractionEvent : HandledEntityEventArgs, IInventoryRelayEvent // Stories
 {
-    public EntityUid Other;
+    public SlotFlags TargetSlots { get; } = SlotFlags.All; // Stories
+    public readonly EntityUid Other;
 
     public ContactInteractionEvent(EntityUid other)
     {
