5015f8740d8cd07a022aace94f1f4d8ee756e58d
Mechanica <skiletik666@gmail.com>
2026-08-11 18:18:10 +0500

    fix spawned entity

--- Content.Server/_Stories/Language/Systems/SpeciesLanguageSystem.cs
+++ Content.Server/_Stories/Language/Systems/SpeciesLanguageSystem.cs
@@ -1,6 +1,8 @@
 using Content.Shared._Stories.Language;
 using Content.Shared._Stories.Language.Prototypes;
 using Content.Shared.GameTicking;
+using Content.Shared.Humanoid;
+using Content.Shared.Humanoid.Prototypes;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._Stories.Language.Systems;
@@ -15,20 +17,31 @@ public sealed partial class SpeciesLanguageSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawnComplete);
+        SubscribeLocalEvent<HumanoidProfileComponent, MapInitEvent>(OnHumanoidMapInit);
     }
 
     private void OnPlayerSpawnComplete(PlayerSpawnCompleteEvent args)
     {
-        if (!_prototype.TryIndex<SpeciesLanguagesPrototype>(args.Profile.Species, out var languages))
+        GrantSpeciesLanguages(args.Mob, args.Profile.Species);
+    }
+
+    private void OnHumanoidMapInit(EntityUid uid, HumanoidProfileComponent component, MapInitEvent args)
+    {
+        GrantSpeciesLanguages(uid, component.Species);
+    }
+
+    private void GrantSpeciesLanguages(EntityUid mob, ProtoId<SpeciesPrototype> species)
+    {
+        if (!_prototype.TryIndex<SpeciesLanguagesPrototype>(species, out var languages))
             return;
 
         foreach (var language in languages.SpokenLanguages)
-            _language.AddLanguage(args.Mob, language, addSpoken: true, addUnderstood: languages.UnderstoodLanguages.Contains(language), source: LanguageSource.Species);
+            _language.AddLanguage(mob, language, addSpoken: true, addUnderstood: languages.UnderstoodLanguages.Contains(language), source: LanguageSource.Species);
 
         foreach (var language in languages.UnderstoodLanguages)
         {
             if (!languages.SpokenLanguages.Contains(language))
-                _language.AddLanguage(args.Mob, language, addSpoken: false, addUnderstood: true, source: LanguageSource.Species);
+                _language.AddLanguage(mob, language, addSpoken: false, addUnderstood: true, source: LanguageSource.Species);
         }
     }
 }
